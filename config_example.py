529f8f6b86e6b16effff1f9b2a0631eaebaad7fd
Laith <62894505+lth17elm@users.noreply.github.com>
2022-01-18 19:36:10 +0100

    :lock: Real secret values normally ignored

---
+++ config_example.py
@@ -0,0 +1,17 @@
+WEBHOOK_PASSPHRASE = "abcdefg"
+
+DISCORD_LOGS_URL = "https://discord.com/api/webhooks/xxxxx"
+DISCORD_ERR_URL = "https://discord.com/api/webhooks/xxxxx"
+DISCORD_AVATAR_URL = "https://imgr.search.brave.com/ZYUP1jUoQ5tr1bxChZtL_sJ0LHz7mDhlhkLHxWxhnPM/fit/680/680/no/1/aHR0cHM6Ly9jbGlw/Z3JvdW5kLmNvbS9p/bWFnZXMvZGlzY29y/ZC1ib3QtbG9nby00/LnBuZw"
+DISCORD_STUDY_URL = "https://discord.com/api/webhooks/xxxxx"
+DISCORD_STUDY_AVATAR_URL = "https://pbs.twimg.com/profile_images/1418656582888525833/p4fZd3KR_400x400.jpg"
+
+LEVERAGE_TESTING = None
+RISK_TESTING = None
+API_KEY_TESTING = None
+API_SECRET_TESTING = None
+
+LEVERAGE_MYBYBITACCOUNT = 50
+RISK_MYBYBITACCOUNT = 5
+API_KEY_MYBYBITACCOUNT = 'xxxxx'
+API_SECRET_MYBYBITACCOUNT = 'xxxxx'
